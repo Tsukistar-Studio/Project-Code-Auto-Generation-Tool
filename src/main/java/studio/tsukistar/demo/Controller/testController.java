9c94b37dcd505b4ee03fbced1be532c7158e472c
Tsukistar <tsukistar@tsukistar.fun>
2023-09-15 14:22:33 +0800

    finish basic springboot project develop

---
+++ src/main/java/studio/tsukistar/demo/Controller/testController.java
@@ -0,0 +1,15 @@
+package studio.tsukistar.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        String hellotext;
+        hellotext = "Hello SpringBoot";
+        return hellotext;
+    }
+}
--- src/main/java/studio/tsukistar/demo/DemoApplication.java
+++ src/main/java/studio/tsukistar/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package studio.tsukistar.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class DemoApplication {
 
 	public static void main(String[] args) {
