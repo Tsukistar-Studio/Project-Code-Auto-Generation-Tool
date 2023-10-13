de5977e9383a2aa90a4327ed40842f05b2b76e87
Tsukistar <tsukistar@tsukistar.fun>
2023-10-13 13:14:28 +0800

    add code select

--- src/main/java/studio/tsukistar/demo/Dao/ProjectInformationDao.java
+++ src/main/java/studio/tsukistar/demo/Dao/ProjectInformationDao.java
@@ -13,11 +13,14 @@ public class ProjectInformationDao {
     }
 
     public void addOperationInformation(ProjectInformationEntity proInfo) { //新增操作记录
-        jdbcTemplate.update("insert into project_information (operation, code, name, category, operation_time) values (?,?,?,?,?)",
-                            proInfo.getOperation(),
-                            proInfo.getCode(),
-                            proInfo.getName(),
-                            proInfo.getCategory(),
-                            proInfo.getTime());
+        String sql = "insert into project_information (operation, code, name, category, operation_time) values (?,?,?,?,?)";
+        jdbcTemplate.update(sql, proInfo.getOperation(), proInfo.getCode(), proInfo.getName(), proInfo.getCategory(), proInfo.getTime());
+    }
+
+    public String selectLatestProjectCode() {
+        String code;
+        String sql = "select code from project_information where operation = '新建编码' order by id desc";
+        code = jdbcTemplate.queryForObject(sql, String.class);
+        return code;
     }
 }
